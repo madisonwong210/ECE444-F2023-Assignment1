77b7765e14e2f736073a2bf9d1f6710018b88256
Madison Wong <madisonwong@madisons-mbp-2.home>
2023-09-13 23:17:55 -0400

    Modify functions to handle negative integers

--- utils.py
+++ utils.py
@@ -1,30 +1,37 @@
 class Utils:
-    def reversed(self, num):
+    @staticmethod
+    def reversed(num):
         if type(num) is not int:
             return "Input must be an int"
         output = ""
         temp = num
+        sign = 1
+        if temp < 0:
+            sign = -1
+            temp = abs(num)
         while temp > 0:
             output += str(temp % 10)
             temp = temp // 10
-        return int(output)
-
-    def formatter(self, num):
+        new = sign * int(output)
+        return new
+    
+    @staticmethod
+    def formatter(num):
         if type(num) is not int:
             return "Input must be an int"
-        # convert to binary
-        temp_bin = num
         binary = ""
-        while temp_bin > 0:
-            binary += str(temp_bin % 2)
-            temp_bin = temp_bin // 2
-
-        # convert to octal
-        temp_oct = num
         octal = ""
-        while temp_oct > 0:
-            octal += str(temp_oct % 8)
-            temp_oct = temp_oct // 8
-        
-        return ("Binary: {} Octal: {}".format(int(binary[::-1]), int(octal[::-1])))
-        
+        for i in [2, 8]:
+            temp = num
+            sign = 1
+            if temp < 0:
+                sign = -1
+                temp = abs(num)    
+            while temp > 0:
+                if i == 2:
+                    binary += str(temp % i)
+                else:
+                    octal += str(temp % i)
+                temp = temp // i
+
+        return ("Binary: {} Octal: {}".format(sign * int(binary[::-1]), sign * int(octal[::-1])))
